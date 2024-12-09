bfcf978f8df1253bc40a0f4920271238e3dd50ba
sc03591 <sc03591@skivecollege.dk>
2024-12-09 11:27:14 +0100

    navmesh VIRKER :DDDDDDDDDDDDDD

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -11,62 +11,60 @@ public class PlayerController : MonoBehaviour
     private Vector2 targetPosition;
     private bool isMoving;
     private Resource targetResource;
-    public bool IsHitting = true;
+    public bool IsHitting = false;
     public float hittingCoolDown = 1f;
-    
+    NavMeshAgent agent;
+    private Vector3 target;
 
     private void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
+
     void Update()
     {
-        if (targetResource == null)
-        {
-            IsHitting = true;
-        }
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            targetPosition = new Vector2(mousePosition.x, mousePosition.y);
+            mousePosition.z = transform.position.z; // Set z-position
+            targetPosition = mousePosition;
             isMoving = true;
-            
 
-            RaycastHit2D hit = Physics2D.Raycast(targetPosition, Vector2.zero);
+            Debug.Log("Mouse Position: " + mousePosition); // Debug
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
             if (hit.collider != null)
             {
-                
+                Debug.Log("Hit Object: " + hit.collider.gameObject.name); // Debug
                 if (hit.collider.gameObject.CompareTag("Resource"))
                 {
                     targetResource = hit.collider.gameObject.GetComponent<Resource>();
                     targetPosition = hit.collider.transform.position;
-                    
                 }
                 else
                 {
                     targetResource = null;
-                    
                 }
             }
             else
             {
+                Debug.Log("No hit detected"); // Debug
                 targetResource = null;
-                
             }
         }
 
         if (isMoving)
         {
-            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-            WiggleEffect();
             
+            //transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            WiggleEffect();
 
             if (targetResource != null)
             {
                 Collider2D resourceCollider = targetResource.GetComponent<Collider2D>();
                 Vector2 closestPoint = resourceCollider.ClosestPoint(transform.position);
                 float distanceToResource = Vector2.Distance(transform.position, closestPoint);
-                
 
                 if (distanceToResource <= interactionDistance)
                 {
@@ -75,11 +73,21 @@ public class PlayerController : MonoBehaviour
                     StartCoroutine(HitResource());
                 }
             }
-            else if (Vector2.Distance(transform.position, targetPosition) <= 0f)
+            else if (Vector2.Distance(transform.position, targetPosition) <= 0.1f)
             {
                 isMoving = false;
                 transform.rotation = Quaternion.identity; // Reset rotation
             }
+        }
+
+        SetAgentDestination();
+    }
+
+    private void SetAgentDestination()
+    {
+        if (isMoving)
+        {
+            agent.SetDestination(new Vector3(targetPosition.x, targetPosition.y, transform.position.z));
         }
     }
 
@@ -91,8 +99,7 @@ public class PlayerController : MonoBehaviour
 
     private IEnumerator HitResource()
     {
-        
-        while (targetResource != null && IsHitting == true)
+        while (targetResource != null)
         {
             IsHitting = false;
             targetResource.Hit();
@@ -101,7 +108,7 @@ public class PlayerController : MonoBehaviour
             yield return new WaitForSeconds(hittingCoolDown); // Adjust hit speed here
             IsHitting = true;
         }
-       
+
         transform.rotation = Quaternion.identity; // Reset rotation after hitting
     }
 
